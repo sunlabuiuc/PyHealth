e2fc6d6b3646b3aa6bc024e2744fee52c7478dc8
programmerXJ <rexjiang@fuji.waseda.jp>
2022-09-07 17:42:15 -0500

    update BaseModel

--- pyhealth/models/modeling_BaseModel.py
+++ pyhealth/models/modeling_BaseModel.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch.nn as nn
 from xgboost import XGBClassifier
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.metrics import log_loss
 from sklearn.model_selection import KFold
@@ -22,18 +22,20 @@ class BaseModel:
         self.y_train = None
         self.X_test = None
         self.X_train = None
+        
+        self.class_num_constraint = 0
 
         # Import model
         if model == 'XGBoost':
             self.model = XGBClassifier(objective='binary:logistic', tree_method='gpu_hist')
         elif model == 'SVM':
-            self.model = LinearSVC()
+            self.model = SVC(kernel='linear',probability=True)
 
         # TODO: Add more models
 
         # For different tasks, we use different loss and output format
         if dataset.task() == 'DrugRec':
-            self.predictor = MultiOutputClassifier(self.model)
+            self.predictor = MultiOutputClassifier(self.model, n_jobs=-1)
 
         # TODO: Add more tasks
 
@@ -94,6 +96,14 @@ class BaseModel:
             print('Starting fold: ', fn)
             X_train_, X_val = self.X_train[trn_idx], self.X_train[val_idx]
             y_train_, y_val = self.y_train[trn_idx], self.y_train[val_idx]
+            
+            # For models such as SVM, LR, at least 2 classes should be detected for each column of trainning data
+            # So, the existence of columns that contain all zeros would lead to an exception
+            # To address this issue, we make a piece of 'unclean' data for training
+            if self.class_num_constraint == 1:
+                for i in range(len(y_train_[0])):
+                    if y_train_[:, i].sum() == 0:
+                        y_train_[0, i] = 1
 
             self.predictor.fit(X_train_, y_train_)
             val_pred = self.predictor.predict_proba(X_val)  # list of preds per class
@@ -109,7 +119,7 @@ class BaseModel:
             print('Mean loss across folds: ', np.mean(val_losses))
             print('STD  loss across folds: ', np.std(val_losses))
 
-    def predict(self, X_test):
+    def predict(self, X_test=None):
         if X_test is None:
             res = self.predictor.predict(self.X_test)
             print('BCE loss: ', log_loss(res, self.y_test))
