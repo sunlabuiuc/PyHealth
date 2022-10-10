23e78e1e482fca8527a7687d04eada5bd4bcbb2a
programmerXJ <rexjiang@fuji.waseda.jp>
2022-10-10 02:28:52 -0500

    minor

--- pyhealth/models/__init__.py
+++ pyhealth/models/__init__.py
@@ -2,9 +2,8 @@ from .base_model import BaseModel
 from .rnn import RNN, RNNLayer
 from .transformer import Transformer, TransformerLayer
 from .retain import RETAIN, RETAINLayer
-
+from .MLModel import MLModel
 # from .MICRON import MICRON
 # from .GAMENet import GAMENet
 # from .SafeDrug import SafeDrug
-# from .MLModel import MLModel
 # from .CNN import CNN
