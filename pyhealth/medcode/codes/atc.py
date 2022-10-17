05c7fb665d0da4682ceb67a504950a160b69e943
zzachw <zw12@illinois.edu>
2022-10-17 12:56:51 -0500

    support mapping to atc any level now

--- pyhealth/medcode/codes/atc.py
+++ pyhealth/medcode/codes/atc.py
@@ -6,6 +6,21 @@ class ATC(InnerMap):
     def __init__(self, **kwargs):
         super(ATC, self).__init__(vocabulary="ATC", **kwargs)
 
+    def postprocess(self, code: str, level=5):
+        if type(level) is str:
+            level = int(level)
+        assert level in [1, 2, 3, 4, 5]
+        if level == 1:
+            return code[:1]
+        elif level == 2:
+            return code[:3]
+        elif level == 3:
+            return code[:4]
+        elif level == 4:
+            return code[:5]
+        else:
+            return code
+
 
 if __name__ == "__main__":
     code_sys = ATC(refresh_cache=True)
