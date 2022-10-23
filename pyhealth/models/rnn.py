b412ad4cd71dba81bb415a87a6c28c9425d4ff76
zzachw <zw12@illinois.edu>
2022-10-22 21:54:05 -0500

    rnn tmp commit

--- pyhealth/models/rnn.py
+++ pyhealth/models/rnn.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Union
+from typing import List
 
 import torch
 import torch.nn as nn
@@ -6,7 +6,6 @@ import torch.nn.utils.rnn as rnn_utils
 
 from pyhealth.datasets import BaseDataset
 from pyhealth.models import BaseModel
-from pyhealth.tokenizer import Tokenizer
 
 
 class RNNLayer(nn.Module):
@@ -70,15 +69,16 @@ class RNNLayer(nn.Module):
         # rnn will only apply dropout between layers
         x = self.dropout_layer(x)
         batch_size = x.size(0)
-        length = torch.sum(mask.int(), dim=-1).cpu()
+        lengths = torch.sum(mask.int(), dim=-1).cpu()
         x = rnn_utils.pack_padded_sequence(
-            x, length, batch_first=True, enforce_sorted=False
+            x, lengths, batch_first=True, enforce_sorted=False
         )
         outputs, _ = self.rnn(x)
         outputs, _ = rnn_utils.pad_packed_sequence(outputs, batch_first=True)
         if self.bidirectional:
             outputs = outputs.view(batch_size, outputs.shape[1], 2, -1)
-            forward_last_outputs = outputs[torch.arange(batch_size), (length - 1), 0, :]
+            forward_last_outputs = outputs[torch.arange(batch_size), (lengths - 1), 0,
+                                   :]
             backward_last_outputs = outputs[:, 0, 1, :]
             last_outputs = torch.cat(
                 [forward_last_outputs, backward_last_outputs], dim=-1
@@ -88,7 +88,7 @@ class RNNLayer(nn.Module):
             outputs = self.down_projection(outputs)
             return last_outputs, outputs
         outputs = self.down_projection(outputs)
-        last_outputs = outputs[torch.arange(batch_size), (length - 1), :]
+        last_outputs = outputs[torch.arange(batch_size), (lengths - 1), :]
         return last_outputs, outputs
 
 
@@ -97,8 +97,8 @@ class RNN(BaseModel):
     
     Args:
         dataset: the dataset object
-        tables: the list of table names to use
-        target: the target table name
+        feature_keys: the list of table names to use
+        label_key: the target table name
         mode: the mode of the model, "multilabel", "multiclass" or "binary"
         embedding_dim: the embedding dimension
         hidden_dim: the hidden dimension
@@ -125,95 +125,101 @@ class RNN(BaseModel):
     def __init__(
             self,
             dataset: BaseDataset,
-            tables: List[str],
-            target: str,
+            feature_keys: List[str],
+            label_key: str,
             mode: str,
+            operation_level: str,
             embedding_dim: int = 128,
             hidden_dim: int = 128,
             **kwargs
     ):
         super(RNN, self).__init__(
             dataset=dataset,
-            tables=tables,
-            target=target,
+            feature_keys=feature_keys,
+            label_key=label_key,
             mode=mode,
         )
+        assert operation_level in ["visit", "event"]
+        self.operation_level = operation_level
         self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
 
-        self.tokenizers = {}
-        for domain in tables:
-            self.tokenizers[domain] = Tokenizer(
-                dataset.get_all_tokens(key=domain), special_tokens=["<pad>", "<unk>"]
-            )
-        self.label_tokenizer = Tokenizer(dataset.get_all_tokens(key=target))
-
-        self.embeddings = nn.ModuleDict()
-        for domain in tables:
-            # TODO: use get_pad_token_id() instead of hard code
-            self.embeddings[domain] = nn.Embedding(
-                self.tokenizers[domain].get_vocabulary_size(),
-                embedding_dim,
-                padding_idx=0,
-            )
+        self.feat_tokenizers = self._get_feature_tokenizers()
+        self.label_tokenizer = self._get_label_tokenizer()
+        self.embeddings = self._get_embeddings(self.feat_tokenizers, embedding_dim)
 
         self.rnn = nn.ModuleDict()
-        for domain in tables:
-            self.rnn[domain] = RNNLayer(
-                input_size=embedding_dim, hidden_size=hidden_dim, **kwargs
+        for feature_key in feature_keys:
+            self.rnn[feature_key] = RNNLayer(
+                input_size=embedding_dim,
+                hidden_size=hidden_dim,
+                **kwargs
             )
-        self.fc = nn.Linear(
-            len(tables) * hidden_dim, self.label_tokenizer.get_vocabulary_size()
-        )
 
-    def forward(self, device, **kwargs):
-        """
-        if "kwargs[domain][0][0] is list" means "use history", then run visit level RNN
-        elif "kwargs[domain][0][0] is not list" means not "use history", then run code level RNN
-        """
+        output_size = self._get_output_size(self.label_tokenizer)
+        self.fc = nn.Linear(len(self.feature_keys) * self.hidden_dim, output_size)
+
+    def _visit_level_forward(self, device, **kwargs):
+        """Visit level RNN forward."""
         patient_emb = []
-        for domain in self.tables:
-            if type(kwargs[domain][0][0]) == list:
-                kwargs[domain] = self.tokenizers[domain].batch_encode_3d(kwargs[domain])
-                kwargs[domain] = torch.tensor(
-                    kwargs[domain], dtype=torch.long, device=device
-                )
-                # (patient, visit, code, embedding_dim)
-                kwargs[domain] = self.embeddings[domain](kwargs[domain])
-                # (patient, visit, embedding_dim)
-                kwargs[domain] = torch.sum(kwargs[domain], dim=2)
-            elif type(kwargs[domain][0][0]) in [int, str]:
-                kwargs[domain] = self.tokenizers[domain].batch_encode_2d(kwargs[domain])
-                kwargs[domain] = torch.tensor(
-                    kwargs[domain], dtype=torch.long, device=device
-                )
-                # (patient, code, embedding_dim)
-                kwargs[domain] = self.embeddings[domain](kwargs[domain])
-            else:
-                raise ValueError("Sample data format is not correct")
-
-            # get mask and run RNN
-            mask = torch.sum(kwargs[domain], dim=2) != 0
-            mask[:, 0] = 1
+        for feature_key in self.feature_keys:
+            assert type(kwargs[feature_key][0][0]) == list
+            x = self.feat_tokenizers[feature_key].batch_encode_3d(kwargs[feature_key])
+            # (patient, visit, code)
+            x = torch.tensor(x, dtype=torch.long, device=device)
+            # (patient, visit, code, embedding_dim)
+            x = self.embeddings[feature_key](x)
+            # (patient, visit, embedding_dim)
+            x = torch.sum(x, dim=2)
+            # (patient, visit)
+            mask = torch.sum(x, dim=2) != 0
             # (patient, hidden_dim)
-            domain_emb, _ = self.rnn[domain](kwargs[domain], mask)
-            patient_emb.append(domain_emb)
-
-        # (patient, hidden_dim * N_tables)
+            x, _ = self.rnn[feature_key](x, mask)
+            patient_emb.append(x)
+        # (patient, features * hidden_dim)
         patient_emb = torch.cat(patient_emb, dim=1)
+        # (patient, label_size)
         logits = self.fc(patient_emb)
-
-        # obtain target, loss, prob, pred
-        loss, y_true, y_prod, y_pred = self.cal_loss_and_output(
-            logits, device, **kwargs
-        )
-
+        # obtain loss, y_true, t_prob
+        loss, y_true, y_prob = self._calculate_output(logits, kwargs[self.label_key])
         return {
             "loss": loss,
-            "y_prob": y_prod,
-            "y_pred": y_pred,
+            "y_prob": y_prob,
             "y_true": y_true,
         }
+
+    def _event_level_forward(self, device, **kwargs):
+        """Event level RNN forward."""
+        patient_emb = []
+        for feature_key in self.feature_keys:
+            x = self.feat_tokenizers[feature_key].batch_encode_2d(kwargs[feature_key])
+            x = torch.tensor(x, dtype=torch.long, device=device)
+            # (patient, code, embedding_dim)
+            x = self.embeddings[feature_key](x)
+            # (patient, code)
+            mask = torch.sum(x, dim=2) != 0
+            # (patient, hidden_dim)
+            x, _ = self.rnn[feature_key](x, mask)
+            patient_emb.append(x)
+        # (patient, features * hidden_dim)
+        patient_emb = torch.cat(patient_emb, dim=1)
+        # (patient, label_size)
+        logits = self.fc(patient_emb)
+        # obtain loss, y_true, t_prob
+        loss, y_true, y_prob = self._calculate_output(logits, kwargs[self.label_key])
+        return {
+            "loss": loss,
+            "y_prob": y_prob,
+            "y_true": y_true,
+        }
+
+    def forward(self, device, **kwargs):
+        if self.operation_level == "visit":
+            return self._visit_level_forward(device, **kwargs)
+        elif self.operation_level == "event":
+            return self._event_level_forward(device, **kwargs)
+        else:
+            raise NotImplementedError
 
 
 if __name__ == '__main__':
@@ -222,14 +228,36 @@ if __name__ == '__main__':
     from pyhealth.utils import collate_fn_dict
 
 
-    def task_fn(patient):
+    def task_event(patient):
         samples = []
         for visit in patient:
-            mortality_label = int(visit.discharge_status)
             conditions = visit.get_code_list(table="DIAGNOSES_ICD")
             procedures = visit.get_code_list(table="PROCEDURES_ICD")
             drugs = visit.get_code_list(table="PRESCRIPTIONS")
-            if len(conditions) + len(procedures) + len(drugs) == 0:
+            mortality_label = int(visit.discharge_status)
+            if len(conditions) * len(procedures) * len(drugs) == 0:
+                continue
+            samples.append(
+                {
+                    "visit_id": visit.visit_id,
+                    "patient_id": patient.patient_id,
+                    "conditions": conditions,
+                    "procedures": procedures,
+                    "list_label": drugs,
+                    "value_label": mortality_label,
+                }
+            )
+        return samples
+
+
+    def task_visit(patient):
+        samples = []
+        for visit in patient:
+            conditions = visit.get_code_list(table="DIAGNOSES_ICD")
+            procedures = visit.get_code_list(table="PROCEDURES_ICD")
+            drugs = visit.get_code_list(table="PRESCRIPTIONS")
+            mortality_label = int(visit.discharge_status)
+            if len(conditions) * len(procedures) * len(drugs) == 0:
                 continue
             samples.append(
                 {
@@ -237,8 +265,8 @@ if __name__ == '__main__':
                     "patient_id": patient.patient_id,
                     "conditions": [conditions],
                     "procedures": [procedures],
-                    "drugs": [drugs],
-                    "label": mortality_label,
+                    "list_label": drugs,
+                    "value_label": mortality_label,
                 }
             )
         return samples
@@ -251,10 +279,105 @@ if __name__ == '__main__':
         code_mapping={"NDC": "ATC"},
         refresh_cache=False,
     )
-    dataset.set_task(task_fn)
 
+    # event level + binary
+    dataset.set_task(task_event)
     dataloader = DataLoader(
         dataset, batch_size=64, shuffle=True, collate_fn=collate_fn_dict
     )
+    model = RNN(
+        dataset=dataset,
+        feature_keys=["conditions", "procedures"],
+        label_key="value_label",
+        mode="binary",
+        operation_level="event",
+    )
+    model.to("cuda")
+    batch = iter(dataloader).next()
+    output = model(**batch, device="cuda")
+    print(output["loss"])
 
-    model = RNN(dataset=dataset, tables=["conditions", "procedures", "drugs"], target="label", mode="binary", embedding_dim=128, hidden_dim=128, rnn_type="GRU", num_layers=1, bidirectional=True, dropout=0.5)
+    # visit level + binary
+    dataset.set_task(task_visit)
+    dataloader = DataLoader(
+        dataset, batch_size=64, shuffle=True, collate_fn=collate_fn_dict
+    )
+    model = RNN(
+        dataset=dataset,
+        feature_keys=["conditions", "procedures"],
+        label_key="value_label",
+        mode="binary",
+        operation_level="visit",
+    )
+    model.to("cuda")
+    batch = iter(dataloader).next()
+    output = model(**batch, device="cuda")
+    print(output["loss"])
+
+    # event level + multiclass
+    dataset.set_task(task_event)
+    dataloader = DataLoader(
+        dataset, batch_size=64, shuffle=True, collate_fn=collate_fn_dict
+    )
+    model = RNN(
+        dataset=dataset,
+        feature_keys=["conditions", "procedures"],
+        label_key="value_label",
+        mode="multiclass",
+        operation_level="event",
+    )
+    model.to("cuda")
+    batch = iter(dataloader).next()
+    output = model(**batch, device="cuda")
+    print(output["loss"])
+
+    # visit level + multiclass
+    dataset.set_task(task_visit)
+    dataloader = DataLoader(
+        dataset, batch_size=64, shuffle=True, collate_fn=collate_fn_dict
+    )
+    model = RNN(
+        dataset=dataset,
+        feature_keys=["conditions", "procedures"],
+        label_key="value_label",
+        mode="multiclass",
+        operation_level="visit",
+    )
+    model.to("cuda")
+    batch = iter(dataloader).next()
+    output = model(**batch, device="cuda")
+    print(output["loss"])
+
+    # event level + multilabel
+    dataset.set_task(task_event)
+    dataloader = DataLoader(
+        dataset, batch_size=64, shuffle=True, collate_fn=collate_fn_dict
+    )
+    model = RNN(
+        dataset=dataset,
+        feature_keys=["conditions", "procedures"],
+        label_key="list_label",
+        mode="multilabel",
+        operation_level="event",
+    )
+    model.to("cuda")
+    batch = iter(dataloader).next()
+    output = model(**batch, device="cuda")
+    print(output["loss"])
+
+    # visit level + multilabel
+    dataset.set_task(task_visit)
+    dataloader = DataLoader(
+        dataset, batch_size=64, shuffle=True, collate_fn=collate_fn_dict
+    )
+    model = RNN(
+        dataset=dataset,
+        feature_keys=["conditions", "procedures"],
+        label_key="list_label",
+        mode="multilabel",
+        operation_level="visit",
+    )
+    model.to("cuda")
+    batch = iter(dataloader).next()
+    output = model(**batch, device="cuda")
+    print(output["loss"])
