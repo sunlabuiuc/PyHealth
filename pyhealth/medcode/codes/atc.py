d2273f7cbc977c87823dffb2055b004fd478afce
zzachw <zw12@illinois.edu>
2022-10-26 17:58:44 -0500

    add ddi to atc

--- pyhealth/medcode/codes/atc.py
+++ pyhealth/medcode/codes/atc.py
@@ -1,12 +1,12 @@
 from pyhealth.medcode.inner_map import InnerMap
+from pyhealth.medcode.utils import download_and_read_csv
 
-
-# TODO: add DDI
 
 class ATC(InnerMap):
 
     def __init__(self, **kwargs):
         super(ATC, self).__init__(vocabulary="ATC", **kwargs)
+        self.ddi = dict()
 
     def postprocess(self, code: str, level=5):
         if type(level) is str:
@@ -23,6 +23,20 @@ class ATC(InnerMap):
         else:
             return code
 
+    @staticmethod
+    def load_ddi(filename: str, refresh_cache: bool = False):
+        df = download_and_read_csv(filename, refresh_cache)
+        ddi = []
+        for idx, row in df.iterrows():
+            ddi.append([row["ATC 1"], row["ATC 2"]])
+        return ddi
+
+    def get_ddi(self, top_40: bool = True, refresh_cache: bool = False):
+        filename = "DDI_TOP40.csv" if top_40 else "DDI_ALL.csv"
+        if filename not in self.ddi or refresh_cache:
+            self.ddi[filename] = self.load_ddi(filename, refresh_cache=refresh_cache)
+        return self.ddi[filename]
+
 
 if __name__ == "__main__":
     code_sys = ATC(refresh_cache=True)
@@ -35,3 +49,5 @@ if __name__ == "__main__":
     print(code_sys.lookup("N01AB07", attribute="drugbank_id"))
     print(code_sys.get_ancestors("N01AB07"))
     print(code_sys.get_descendants("N01AB"))
+    print(len(code_sys.get_ddi(top_40=True)))
+    print(len(code_sys.get_ddi(top_40=False)))
