c2a52601592ca13c335687f5d8e63bbcbe335e50
ycq091044 <ycqsjtu@gmail.com>
2022-10-26 11:35:41 -0500

    add doc examples for pyhealth.tasks

--- pyhealth/tasks/length_of_stay_prediction.py
+++ pyhealth/tasks/length_of_stay_prediction.py
@@ -44,6 +44,19 @@ def length_of_stay_prediction_mimic3_fn(patient: Patient):
             and other task-specific attributes as key.
 
     Note that we define the task as a multi-class classification task.
+    
+    **Example:**
+        >>> from pyhealth.datasets import MIMIC3Dataset
+        >>> mimic3_ds = MIMIC3Dataset(
+        ...    root="/srv/local/data/physionet.org/files/mimiciii/1.4",
+        ...    tables=["DIAGNOSES_ICD", "PROCEDURES_ICD", "PRESCRIPTIONS"],
+        ...    code_mapping={"ICD9CM": "CCSCM"},
+        ... )
+        >>> from pyhealth.tasks import length_of_stay_prediction_mimic3_fn
+        >>> dataset.set_task(length_of_stay_prediction_mimic3_fn) # set task
+        >>> dataset.samples[0] # exampe of an training sample
+        [{'visit_id': '130744', 'patient_id': '103', 'conditions': [['42', '109', '19', '122', '98', '663', '58', '51']], 'procedures': [['1']], 'label': 4}]
+        
     """
     samples = []
 
@@ -90,6 +103,20 @@ def length_of_stay_prediction_mimic4_fn(patient: Patient):
             and other task-specific attributes as key.
 
     Note that we define the task as a multi-class classification task.
+    
+    **Example:**
+        >>> from pyhealth.datasets import MIMIC4Dataset
+        >>> mimic4_ds = MIMIC4Dataset(
+        ...     root="/srv/local/data/physionet.org/files/mimiciv/2.0/hosp",
+        ...     tables=["diagnoses_icd", "procedures_icd"],
+        ...     code_mapping={"ICD10PROC": "CCSPROC"},
+        ... )
+        >>> from pyhealth.tasks import length_of_stay_prediction_mimic4_fn
+        >>> dataset.set_task(length_of_stay_prediction_mimic4_fn) # set task
+        >>> dataset.samples[0] # exampe of an training sample
+        [{'visit_id': '130744', 'patient_id': '103', 'conditions': [['42', '109', '19', '122', '98', '663', '58', '51']], 'procedures': [['1']], 'label': 2}]
+        
+        
     """
     samples = []
 
@@ -136,6 +163,21 @@ def length_of_stay_prediction_eicu_fn(patient: Patient):
             and other task-specific attributes as key.
 
     Note that we define the task as a multi-class classification task.
+    
+    **Example:**
+        >>> from pyhealth.datasets import eICUDataset
+        >>> eicu_ds = eICUDataset(
+        ...     root="/srv/local/data/physionet.org/files/eicu-crd/2.0",
+        ...     tables=["diagnosis", "medication"],
+        ...     code_mapping={},
+        ...     dev=True
+        ... )
+        >>> from pyhealth.tasks import length_of_stay_prediction_eicu_fn
+        >>> dataset.set_task(length_of_stay_prediction_eicu_fn) # set task
+        >>> dataset.samples[0] # exampe of an training sample
+        [{'visit_id': '130744', 'patient_id': '103', 'conditions': [['42', '109', '98', '663', '58', '51']], 'procedures': [['1']], 'label': 5}]
+        
+        
     """
     samples = []
 
@@ -182,6 +224,19 @@ def length_of_stay_prediction_omop_fn(patient: Patient):
             and other task-specific attributes as key.
 
     Note that we define the task as a multi-class classification task.
+    
+    **Examples:**
+        >>> from pyhealth.datasets import OMOPDataset
+        >>> omop_ds = OMOPDataset(
+        ...     root="https://storage.googleapis.com/pyhealth/synpuf1k_omop_cdm_5.2.2",
+        ...     tables=["condition_occurrence", "procedure_occurrence"],
+        ...     code_mapping={},
+        ... )
+        >>> from pyhealth.tasks import length_of_stay_prediction_omop_fn
+        >>> dataset.set_task(length_of_stay_prediction_eicu_fn) # set task
+        >>> dataset.samples[0] # exampe of an training sample
+        [{'visit_id': '130744', 'patient_id': '103', 'conditions': [['42', '109', '98', '663', '58', '51']], 'procedures': [['1']], 'label': 7}]
+        
     """
     samples = []
 
