3e1edb13498f474daa7fe9c5113fe3384b19c7f2
zzachw <zw12@illinois.edu>
2022-11-11 01:50:22 -0600

    stat bug fixed for empty list

--- pyhealth/datasets/utils.py
+++ pyhealth/datasets/utils.py
@@ -1,6 +1,7 @@
 import hashlib
 import os
 from datetime import datetime
+from typing import List
 from typing import Optional
 
 from dateutil.parser import parse as dateutil_parse
@@ -32,6 +33,36 @@ def strptime(s: str) -> Optional[datetime]:
     return dateutil_parse(s)
 
 
+def list_nested_level(l: List) -> int:
+    """Gets the nested level of a list.
+
+    Args:
+        l: List, the list to be checked.
+
+    Returns:
+        int, the nested level of the list.
+    """
+    if not isinstance(l, list):
+        return 0
+    if not l:
+        return 1
+    return 1 + max(list_nested_level(i) for i in l)
+
+
+def is_homo_list(l: List) -> bool:
+    """Checks if a list is homogeneous.
+
+    Args:
+        l: the list to be checked.
+
+    Returns:
+        bool, True if the list is homogeneous, False otherwise.
+    """
+    if not l:
+        return True
+    return all(isinstance(i, type(l[0])) for i in l)
+
+
 def collate_fn_dict(batch):
     return {key: [d[key] for d in batch] for key in batch[0]}
 
@@ -41,3 +72,12 @@ def get_dataloader(dataset, batch_size, shuffle=False):
         dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn_dict
     )
     return dataloader
+
+
+if __name__ == "__main__":
+    print(list_nested_level([1, 2, 3]))
+    print(list_nested_level([1, [2], 3]))
+    print(list_nested_level([1, [2], [[3]]]))
+    print(is_homo_list([1, 2, 3]))
+    print(is_homo_list([1, 2, [3]]))
+    print(is_homo_list([1, 2.0]))
