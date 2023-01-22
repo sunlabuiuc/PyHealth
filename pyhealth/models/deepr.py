0c4336f45b62767ea5bd3f99691d4b2527d250de
ycq091044 <ycqsjtu@gmail.com>
2023-01-22 12:05:24 -0600

    add examples to deepr

--- pyhealth/models/deepr.py
+++ pyhealth/models/deepr.py
@@ -113,6 +113,65 @@ class Deepr(BaseModel):
         embedding_dim: the embedding dimension. Default is 128.
         hidden_dim: the hidden dimension. Default is 128.
         **kwargs: other parameters for the Deepr layer.
+
+    Examples:
+        >>> from pyhealth.datasets import SampleDataset
+        >>> samples = [
+        ...         {
+        ...             "patient_id": "patient-0",
+        ...             "visit_id": "visit-0",
+        ...             "list_codes": ["505800458", "50580045810", "50580045811"],  # NDC
+        ...             "list_vectors": [[1.0, 2.55, 3.4], [4.1, 5.5, 6.0]],
+        ...             "list_list_codes": [["A05B", "A05C", "A06A"], ["A11D", "A11E"]],  # ATC-4
+        ...             "list_list_vectors": [
+        ...                 [[1.8, 2.25, 3.41], [4.50, 5.9, 6.0]],
+        ...                 [[7.7, 8.5, 9.4]],
+        ...             ],
+        ...             "label": 1,
+        ...         },
+        ...         {
+        ...             "patient_id": "patient-0",
+        ...             "visit_id": "visit-1",
+        ...             "list_codes": [
+        ...                 "55154191800",
+        ...                 "551541928",
+        ...                 "55154192800",
+        ...                 "705182798",
+        ...                 "70518279800",
+        ...             ],
+        ...             "list_vectors": [[1.4, 3.2, 3.5], [4.1, 5.9, 1.7], [4.5, 5.9, 1.7]],
+        ...             "list_list_codes": [["A04A", "B035", "C129"]],
+        ...             "list_list_vectors": [
+        ...                 [[1.0, 2.8, 3.3], [4.9, 5.0, 6.6], [7.7, 8.4, 1.3], [7.7, 8.4, 1.3]],
+        ...             ],
+        ...             "label": 0,
+        ...         },
+        ...     ]
+        >>> dataset = SampleDataset(samples=samples, dataset_name="test")
+        >>>
+        >>> from pyhealth.models import Deepr
+        >>> model = Deepr(
+        ...         dataset=dataset,
+        ...         feature_keys=[
+        ...             "list_list_codes",
+        ...             "list_list_vectors",
+        ...         ],
+        ...         label_key="label",
+        ...         mode="binary",
+        ...     )
+        >>>
+        >>> from pyhealth.datasets import get_dataloader
+        >>> train_loader = get_dataloader(dataset, batch_size=2, shuffle=True)
+        >>> data_batch = next(iter(train_loader))
+        >>>
+        >>> ret = model(**data_batch)
+        >>> print(ret)
+        {'loss': tensor(0.9139, device='cuda:0',
+            grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'y_prob': tensor([[0.7530],
+                [0.6510]], device='cuda:0', grad_fn=<SigmoidBackward0>), 'y_true': tensor([[0.],
+                [1.]], device='cuda:0')}
+        >>>
+
     """
 
     def __init__(
@@ -239,7 +298,7 @@ if __name__ == "__main__":
         {
             "patient_id": "patient-0",
             "visit_id": "visit-0",
-            # "single_vector": [1, 2, 3],
+            "single_vector": [1, 2, 3],
             "list_codes": ["505800458", "50580045810", "50580045811"],  # NDC
             "list_vectors": [[1.0, 2.55, 3.4], [4.1, 5.5, 6.0]],
             "list_list_codes": [["A05B", "A05C", "A06A"], ["A11D", "A11E"]],  # ATC-4
@@ -252,7 +311,7 @@ if __name__ == "__main__":
         {
             "patient_id": "patient-0",
             "visit_id": "visit-1",
-            # "single_vector": [1, 5, 8],
+            "single_vector": [1, 5, 8],
             "list_codes": [
                 "55154191800",
                 "551541928",
