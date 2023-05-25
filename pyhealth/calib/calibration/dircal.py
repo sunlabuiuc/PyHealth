f5c1efa64f7c29130fcd0b8efa99d5f23c63bb90
ycq091044 <ycqsjtu@gmail.com>
2023-05-25 16:10:08 -0500

    diracl: create examples for quick test

--- pyhealth/calib/calibration/dircal.py
+++ pyhealth/calib/calibration/dircal.py
@@ -16,11 +16,12 @@ from pyhealth.calib.utils import prepare_numpy_dataset
 from pyhealth.models import BaseModel
 
 
-def _get_identity_weights(n_classes, method='Full'):
+def _get_identity_weights(n_classes, method="Full"):
     raw_weights = None
-    if (method is None) or (method == 'Full'):
-        raw_weights = torch.zeros((n_classes, n_classes + 1)) + \
-                      torch.hstack([torch.eye(n_classes), torch.zeros((n_classes, 1))])
+    if (method is None) or (method == "Full"):
+        raw_weights = torch.zeros((n_classes, n_classes + 1)) + torch.hstack(
+            [torch.eye(n_classes), torch.zeros((n_classes, 1))]
+        )
     else:
         raise NotImplementedError
     return raw_weights.ravel()
@@ -32,18 +33,20 @@ def _softmax(X):
     exps = torch.exp(shiftx)
     return exps / torch.sum(exps, axis=1).reshape(-1, 1)
 
-def _get_weights(params, k, ref_row, method):
-        ''' Reshapes the given params (weights) into the full matrix including 0
-        '''
 
-        if method in ['Full', None]:
-            raw_weights = params.reshape(-1, k+1)
-        if ref_row:
-            weights = raw_weights - torch.repeat_interleave(
-                raw_weights[-1, :].reshape(1, -1), k, dim=0)
-        else:
-            weights = raw_weights
-        return weights
+def _get_weights(params, k, ref_row, method):
+    """Reshapes the given params (weights) into the full matrix including 0"""
+
+    if method in ["Full", None]:
+        raw_weights = params.reshape(-1, k + 1)
+    if ref_row:
+        weights = raw_weights - torch.repeat_interleave(
+            raw_weights[-1, :].reshape(1, -1), k, dim=0
+        )
+    else:
+        weights = raw_weights
+    return weights
+
 
 class DirichletCalibration(PostHocCalibrator):
     """Dirichlet Calibration
@@ -64,12 +67,13 @@ class DirichletCalibration(PostHocCalibrator):
     :type model: BaseModel
 
     Examples:
+        >>> from pyhealth.datasets import ISRUCDataset, split_by_patient, get_dataloader
         >>> from pyhealth.models import SparcNet
         >>> from pyhealth.tasks import sleep_staging_isruc_fn
         >>> from pyhealth.calib.calibration import DirichletCalibration
         >>> sleep_ds = ISRUCDataset("/srv/scratch1/data/ISRUC-I").set_task(sleep_staging_isruc_fn)
         >>> train_data, val_data, test_data = split_by_patient(sleep_ds, [0.6, 0.2, 0.2])
-        >>> model = SparcNet(dataset=sleep_staging_ds, feature_keys=["signal"],
+        >>> model = SparcNet(dataset=sleep_ds, feature_keys=["signal"],
         ...     label_key="label", mode="multiclass")
         >>> # ... Train the model here ...
         >>> # Calibrate
@@ -81,7 +85,8 @@ class DirichletCalibration(PostHocCalibrator):
         >>> print(Trainer(model=cal_model, metrics=['cwECEt_adapt', 'accuracy']).evaluate(test_dl))
         {'accuracy': 0.7096615988229524, 'cwECEt_adapt': 0.05336195546573208}
     """
-    def __init__(self, model:BaseModel, debug=False, **kwargs) -> None:
+
+    def __init__(self, model: BaseModel, debug=False, **kwargs) -> None:
         super().__init__(model, **kwargs)
         self.mode = self.model.mode
         for param in model.parameters():
@@ -95,13 +100,16 @@ class DirichletCalibration(PostHocCalibrator):
         self.weights = None
 
     def _forward(self, logits):
-        logits = torch.hstack([logits, torch.zeros((logits.shape[0], 1), device=self.device)])
-        weights = _get_weights(self._weights, k=self.num_classes, ref_row=True, method='Full')
-        new_logits = torch.matmul(logits, weights.permute(1,0))
+        logits = torch.hstack(
+            [logits, torch.zeros((logits.shape[0], 1), device=self.device)]
+        )
+        weights = _get_weights(
+            self._weights, k=self.num_classes, ref_row=True, method="Full"
+        )
+        new_logits = torch.matmul(logits, weights.permute(1, 0))
         return new_logits, weights
 
-
-    def calibrate(self, cal_dataset:Subset, lr=0.01, max_iter=128, reg_lambda=1e-3):
+    def calibrate(self, cal_dataset: Subset, lr=0.01, max_iter=128, reg_lambda=1e-3):
         """Calibrate the base model using a calibration dataset.
 
         :param cal_dataset: Calibration set.
@@ -117,39 +125,63 @@ class DirichletCalibration(PostHocCalibrator):
         :rtype: None
         """
 
-        _cal_data = prepare_numpy_dataset(self.model, cal_dataset,
-                                          ['y_true', 'logit'], debug=self.debug)
+        _cal_data = prepare_numpy_dataset(
+            self.model, cal_dataset, ["y_true", "logit"], debug=self.debug
+        )
         if self.num_classes is None:
-            self.num_classes = _cal_data['logit'].shape[1]
+            self.num_classes = _cal_data["logit"].shape[1]
 
-        self._weights = torch.tensor(_get_identity_weights(self.num_classes), device=self.device, requires_grad=True)
+        self._weights = torch.tensor(
+            _get_identity_weights(self.num_classes),
+            device=self.device,
+            requires_grad=True,
+        )
         optimizer = optim.LBFGS([self._weights], lr=lr, max_iter=max_iter)
-        logits = torch.tensor(_cal_data['logit'], dtype=torch.float, device=self.device)
-        label = torch.tensor(F.one_hot(torch.tensor(_cal_data['y_true']), num_classes=self.num_classes),
-                             dtype=torch.long if self.model.mode == 'multiclass' else torch.float32,
-                             device=self.device)
+        logits = torch.tensor(_cal_data["logit"], dtype=torch.float, device=self.device)
+        label = torch.tensor(
+            F.one_hot(torch.tensor(_cal_data["y_true"]), num_classes=self.num_classes),
+            dtype=torch.long if self.model.mode == "multiclass" else torch.float32,
+            device=self.device,
+        )
 
         reg_mu = None
-        reg_format = 'identity'
+        reg_format = "identity"
+
         def _eval():
             optimizer.zero_grad()
             new_logits, weights = self._forward(logits)
             probs = _softmax(new_logits)
             loss = torch.mean(-torch.log(torch.sum(label * probs, dim=1)))
             if reg_mu is None:
-                if reg_format == 'identity':
-                    reg = torch.hstack([torch.eye(self.num_classes), torch.zeros((self.num_classes, 1))])
+                if reg_format == "identity":
+                    reg = torch.hstack(
+                        [
+                            torch.eye(self.num_classes),
+                            torch.zeros((self.num_classes, 1)),
+                        ]
+                    )
                 else:
-                    reg = torch.zeros((self.num_classes, self.num_classes+1))
-                loss = loss + reg_lambda * torch.sum((weights - reg.to(self.device))**2)
+                    reg = torch.zeros((self.num_classes, self.num_classes + 1))
+                loss = loss + reg_lambda * torch.sum(
+                    (weights - reg.to(self.device)) ** 2
+                )
             else:
-                weights_hat = weights - torch.hstack([weights[:, :-1] * torch.eye(self.num_classes, device=self.device),
-                                                torch.zeros((self.num_classes, 1), device=self.device)])
-                loss = loss + reg_lambda * torch.sum(weights_hat[:, :-1] ** 2) + \
-                    reg_mu * torch.sum(weights_hat[:, -1] ** 2)
+                weights_hat = weights - torch.hstack(
+                    [
+                        weights[:, :-1]
+                        * torch.eye(self.num_classes, device=self.device),
+                        torch.zeros((self.num_classes, 1), device=self.device),
+                    ]
+                )
+                loss = (
+                    loss
+                    + reg_lambda * torch.sum(weights_hat[:, :-1] ** 2)
+                    + reg_mu * torch.sum(weights_hat[:, -1] ** 2)
+                )
 
             loss.backward()
             return loss
+
         self.train()
         optimizer.step(_eval)
         self.eval()
@@ -167,8 +199,36 @@ class DirichletCalibration(PostHocCalibrator):
         :rtype: Dict[str, torch.Tensor]
         """
         ret = self.model(**kwargs)
-        ret['logit'] = self._forward(ret['logit'])[0]
-        ret['y_prob'] = self.model.prepare_y_prob(ret['logit'])
+        ret["logit"] = self._forward(ret["logit"])[0]
+        ret["y_prob"] = self.model.prepare_y_prob(ret["logit"])
         criterion = self.model.get_loss_function()
-        ret['loss'] = criterion(ret['logit'], ret['y_true'])
+        ret["loss"] = criterion(ret["logit"], ret["y_true"])
         return ret
+
+
+if __name__ == "__main__":
+
+    from pyhealth.datasets import ISRUCDataset, split_by_patient, get_dataloader
+    from pyhealth.models import SparcNet
+    from pyhealth.tasks import sleep_staging_isruc_fn
+    from pyhealth.calib.calibration import DirichletCalibration
+
+    sleep_ds = ISRUCDataset(
+        root="/srv/local/data/trash/",
+        dev=True,
+    ).set_task(sleep_staging_isruc_fn)
+    train_data, val_data, test_data = split_by_patient(sleep_ds, [0.6, 0.2, 0.2])
+    model = SparcNet(
+        dataset=sleep_ds, feature_keys=["signal"], label_key="label", mode="multiclass"
+    )
+    # ... Train the model here ...
+    # Calibrate
+    cal_model = DirichletCalibration(model)
+    cal_model.calibrate(cal_dataset=val_data)
+    # Evaluate
+    from pyhealth.trainer import Trainer
+
+    test_dl = get_dataloader(test_data, batch_size=32, shuffle=False)
+    print(
+        Trainer(model=cal_model, metrics=["cwECEt_adapt", "accuracy"]).evaluate(test_dl)
+    )
