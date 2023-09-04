f95eb45a226dd83bfae42a395f71311e3afe4789
weicong.tan@monash.edu <wtan0047@m3a103.massive.org.au>
2023-09-04 13:46:14 +1000

    Add back the ethnicity column

--- pyhealth/datasets/mimic4.py
+++ pyhealth/datasets/mimic4.py
@@ -117,6 +117,7 @@ class MIMIC4Dataset(BaseEHRDataset):
                 # no exact time, use 00:00:00
                 death_datetime=strptime(p_info["dod"].values[0]),
                 gender=p_info["gender"].values[0],
+                ethnicity=p_info["ethnicity"].values[0],
                 anchor_year_group=p_info["anchor_year_group"].values[0],
             )
             # load visits
