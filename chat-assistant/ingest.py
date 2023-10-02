c6e85d430df4a49f7dfea47db23ee7fa088e7ba5
windszzlang <windszzlang@gmail.com>
2023-10-01 21:07:42 -0500

    better chatbot

--- chat-assistant/ingest.py
+++ chat-assistant/ingest.py
@@ -8,23 +8,32 @@ from env import OPENAI_API_KEY
 
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-print("Loading data...")
-loader = UnstructuredFileLoader("pyhealth.txt")
-raw_documents = loader.load()
+
+def vectorize_corpus(corpus_path):
+    print(f"Vectorizing {corpus_path}...")
+    # print(f"Loading data {corpus_path}...")
+    loader = UnstructuredFileLoader(corpus_path)
+    raw_documents = loader.load()
 
 
-print("Splitting text...")
-text_splitter = CharacterTextSplitter(
-    separator="\n\n",
-    chunk_size=600,
-    chunk_overlap=100,
-    length_function=len,
-)
-documents = text_splitter.split_documents(raw_documents)
+    # print("Splitting text...")
+    text_splitter = CharacterTextSplitter(
+        separator="\n\n",
+        chunk_size=600,
+        chunk_overlap=100,
+        length_function=len,
+    )
+    documents = text_splitter.split_documents(raw_documents)
+
+    # print("Creating vectorstore...")
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(documents, embeddings)
+
+    base_name, ext = os.path.splitext(corpus_path)
+    vectorstore_path = base_name + '.pkl'
+    with open(vectorstore_path, "wb") as f:
+        pickle.dump(vectorstore, f)
 
 
-print("Creating vectorstore...")
-embeddings = OpenAIEmbeddings()
-vectorstore = FAISS.from_documents(documents, embeddings)
-with open("vectorstore.pkl", "wb") as f:
-    pickle.dump(vectorstore, f)
+vectorize_corpus('corpus/pyhealth-text.txt')
+vectorize_corpus('corpus/pyhealth-code.txt')
