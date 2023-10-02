b748cae5b25d1d8a8e4b916759bd9179df05a8f4
Jathurshan Pradeepkumar <jp65@sunlab-work-01.cs.illinois.edu>
2023-10-02 00:31:55 -0500

    Added many-to-many sleep classification task

--- pyhealth/tasks/sleep_staging.py
+++ pyhealth/tasks/sleep_staging.py
@@ -329,6 +329,149 @@ def sleep_staging_shhs_fn(record, epoch_seconds=30):
     return samples
 
 
+def multi_epoch_multi_modal_sleep_staging_sleepedf_fn(record, epoch_seconds=30,modality = ['EEG Fpz-Cz','EOG horizontal'],num_epoch_seq = 15,is_filter = True):
+    """Processes a single patient for the many-to-many sleep staging task on Sleep EDF.
+
+    Sleep staging aims at predicting the sleep stages (Awake, REM, N1, N2, N3, N4) based on
+    the multi-modal polysomnography signals (EEG, EOG,..). The task is defined as a multi-class classification.
+    Particularly, this task focuses on many-to-many classification scheme, where the input is a sequence of epochs and 
+    the model predicts the sleep stages of all the epochs in the sequence at the same time. This task is defined as a 
+    many-to-many multi-class classification task.
+
+    Args:
+        patient: a list of (load_from_path, signal_file, label_file, save_to_path) tuples, where PSG is the signal files and the labels are
+        in label file
+        epoch_seconds: how long will each epoch be (in seconds)
+        modality: the modalities to be used for the task, default is EEG and EOG
+        num_epoch_seq: the number of epochs in the sequence, default is 15
+        is_filter: whether to apply the bandpass filtering to the PSG signals and conduct subject-wise normalization, default is True
+
+    Returns:
+        samples: a list of samples, each sample is a dict with patient_id, record_id,
+            and epoch_path (the path to the saved epoch {"X": signal, "Y": label} as key.
+            "X" is a array of selected PSG signals, with shape (num_channels,num_epoch_seq*epoch_seconds*sample_rate)
+            "Y" is a array of tokenized labels for a sequence of epochs, with shape (num_epoch_seq,)
+
+    Note that we define the task as a many-to-many multi-class classification task.
+
+    Examples:
+        >>> from pyhealth.datasets import SleepEDFDataset
+        >>> sleepedf = SleepEDFDataset(
+        ...         root="/srv/local/data/SLEEPEDF/sleep-edf-database-expanded-1.0.0/sleep-cassette",
+        ...     )
+        >>> from pyhealth.tasks import multi_epoch_multi_modal_sleep_staging_sleepedf_fn
+        >>> modality = ['EEG Fpz-Cz','EOG horizontal']
+        >>> num_epoch_seq = 15
+        >>> sleepstage_ds = sleepedf.set_task(lambda x: multi_epoch_multi_modal_sleep_staging_sleepedf_fn(x,modality = modality,num_epoch_seq = num_epoch_seq))
+        >>> sleepstage_ds.samples[0]
+        {'record_id': 'SC4002-1860_1861_1862_1863_1864',
+         'patient_id': 'SC4002',
+         'epoch_path': '/home/jp65/.cache/pyhealth/datasets/03689253dc58603acb33b904c30ad5a0/SC4002-1860_1861_1862_1863_1864.pkl',
+         'label': array([2, 2, 2, 2, 1])
+        }
+    """
+    ALL_CHANNELS = ['EEG Fpz-Cz', 'EEG Pz-Oz', 'EOG horizontal', 'EMG submental', 'Resp oro-nasal', 'Temp rectal', 'Event marker']
+    exclude_channels = [ch for ch in ALL_CHANNELS if ch not in modality]
+    exclude_channels = tuple(exclude_channels)
+
+    SAMPLE_RATE = 100
+
+    root, psg_file, hypnogram_file, save_path = (
+        record[0]["load_from_path"],
+        record[0]["signal_file"],
+        record[0]["label_file"],
+        record[0]["save_to_path"],
+    )
+    # get patient id
+    pid = psg_file[:6]
+
+    # load signal "X" part and exclude the channels not in the modality
+    data = mne.io.read_raw_edf(os.path.join(root, psg_file),exclude=exclude_channels)
+
+    X = data.get_data()
+
+    #Apply filter and normalization
+    if is_filter:
+        X[0,:] = mne.filter.filter_data(X[0,:], SAMPLE_RATE, 0.5, 40, verbose=False)
+        X[0,:] = X[0,:]/np.max(X[0,:])
+        X[1,:] = mne.filter.filter_data(X[1,:], SAMPLE_RATE, 0.5, 40, verbose=False)
+        X[1,:] = X[1,:]/np.max(X[1,:])
+
+    # load label "Y" part
+    ann = mne.read_annotations(os.path.join(root, hypnogram_file))
+
+    labels = []
+    for dur, des in zip(ann.duration, ann.description):
+        """
+        all possible des:
+            - 'Sleep stage W'
+            - 'Sleep stage 1'
+            - 'Sleep stage 2'
+            - 'Sleep stage 3'
+            - 'Sleep stage 4'
+            - 'Sleep stage R'
+            - 'Sleep stage ?'
+            - 'Movement time'
+        """
+        for _ in range(int(dur) // 30):
+            labels.append(des)
+
+    samples = []
+    sample_length = SAMPLE_RATE * epoch_seconds
+
+    # Extract sequence of epochs and labels
+    # slice the EEG signals into non-overlapping windows
+    # window size = sampling rate * second time = 100 * epoch_seconds
+    all_labels = ["Sleep stage W",
+                "Sleep stage 1",
+                "Sleep stage 2",
+                "Sleep stage 3",
+                "Sleep stage 4",
+                "Sleep stage R"]
+    for slice_index in range(min((X.shape[1] // sample_length)-num_epoch_seq , len(labels))):   ### I edited this line
+        # igore the no label epoch sequence
+        flag = False
+        for seq_ep_no in range(num_epoch_seq):
+            if labels[slice_index+seq_ep_no] not in all_labels:
+                flag = True
+                break
+        if flag:
+            continue
+        
+        # Extract the sequence of epochs in the shape of (num_channels,num_epoch_seq*epoch_seconds*sample_rate)
+        epoch_signal = X[
+            :, slice_index * sample_length : (slice_index + seq_ep_no+1) * sample_length
+        ]
+        # Extract the sequence of labels in the shape of (num_epoch_seq,)
+        epoch_label = np.array([
+                all_labels.index(labels[slice_index+seq_ep_no])
+                for seq_ep_no in range(num_epoch_seq)
+            ])
+        
+        # Save the sequence of epochs and labels in the format of {pid}-{slice_ids of epochs in the sequence}.pkl
+        slice_ids = [str(slice_index+seq_ep_no) for seq_ep_no in range(num_epoch_seq)]
+        slice_ids = '_'.join(slice_ids)
+        save_file_path = os.path.join(save_path, f"{pid}-{slice_ids}.pkl")
+
+        pickle.dump(
+            {
+                "signal": epoch_signal,
+                "label": epoch_label,
+            },
+            open(save_file_path, "wb"),
+        )
+
+        samples.append(
+            {
+                "record_id": f"{pid}-{slice_ids}",
+                "patient_id": pid,
+                "epoch_path": save_file_path,
+                "label": epoch_label,  # use for counting the label tokens
+            }
+        )
+    return samples
+
+
 if __name__ == "__main__":
     from pyhealth.datasets import SleepEDFDataset, SHHSDataset, ISRUCDataset
 
@@ -358,6 +501,23 @@ if __name__ == "__main__":
     # # print(sleep_staging_ds.patient_to_index)
     # # print(sleep_staging_ds.record_to_index)
     # print(sleep_staging_ds.input_info)
+
+
+    ############################
+    # """ test many-to-many sleep edf"""
+    # dataset = SleepEDFDataset(
+    #     root="/srv/local/data/SLEEPEDF/sleep-edf-database-expanded-1.0.0/sleep-telemetry",
+    #     dev=True,
+    #     refresh_cache=True,
+    # )
+    # modality = ['EEG Fpz-Cz','EOG horizontal']
+    # num_epoch_seq = 15
+    # sleep_staging_ds = dataset.set_task(lambda x: multi_epoch_multi_modal_sleep_staging_sleepedf_fn(x,modality = modality,num_epoch_seq = num_epoch_seq))
+    # print(sleep_staging_ds.samples[0])
+    # print(sleep_staging_ds.input_info)
+
+
+
     
     dataset = SHHSDataset(
         root="/srv/local/data/SHHS/polysomnography",
