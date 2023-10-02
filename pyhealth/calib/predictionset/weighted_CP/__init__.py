f345e686cf01c2bef07833f7f110eb8a1a0c4495
Siddhartha Laghuvarapu <sl160@sunlab-serv-02.cs.illinois.edu>
2023-10-02 11:49:26 -0500

    Add preliminary code for Weighted CP and CoDrug

--- pyhealth/calib/predictionset/weighted_CP/__init__.py
+++ pyhealth/calib/predictionset/weighted_CP/__init__.py
@@ -1,15 +1,13 @@
 """
-LABEL: Least ambiguous set-valued classifiers with bounded error levels.
-
-Paper:
-
-    Sadinle, Mauricio, Jing Lei, and Larry Wasserman.
-    "Least ambiguous set-valued classifiers with bounded error levels."
-    Journal of the American Statistical Association 114, no. 525 (2019): 223-234.
+WeightedCP: Weighted Conformal Prediction. Conformal Prediction with differnt weights to calibration samples. Base class for:
+Conformal Prediction Under Covariate Shift
+Locally Valid Discrminative Intervals
+CoDrug: Conformal Drug Property Prediction with Denisty Estimation Under Covariate Shift
 
 """
 
 from typing import Dict, Union
+from typing import overload
 
 import numpy as np
 import torch
@@ -18,6 +16,11 @@ from torch.utils.data import Subset
 from pyhealth.calib.base_classes import SetPredictor
 from pyhealth.calib.utils import prepare_numpy_dataset
 from pyhealth.models import BaseModel
+
+import logging
+import bisect
+
+from pyhealth.calib.utils import compute_weights_for_codrug 
 
 __all__ = ["LABEL"]
 
@@ -28,8 +31,12 @@ def _query_quantile(scores, alpha):
     loc = int(np.floor(alpha * (N + 1))) - 1
     return -np.inf if loc == -1 else scores[loc]
 
+def _query_weighted_quantile(scores, alpha, weights):
+    qs = np.cumsum(weights)/np.sum(weights)
+    idx = bisect.bisect_left(qs, alpha, lo=0, hi=len(qs)-1)
+    return scores[idx]
 
-class LABEL(SetPredictor):
+class WeightedCP(SetPredictor):
     """LABEL: Least ambiguous set-valued classifiers with bounded error levels.
 
     This is a prediction-set constructor for multi-class classification problems.
@@ -79,7 +86,11 @@ class LABEL(SetPredictor):
     """
 
     def __init__(
-        self, model: BaseModel, alpha: Union[float, np.ndarray], debug=False, **kwargs
+        self, 
+        model: BaseModel, 
+        alpha: Union[float, np.ndarray], 
+        debug=False,
+         **kwargs
     ) -> None:
         super().__init__(model, **kwargs)
         if model.mode != "multiclass":
@@ -90,33 +101,45 @@ class LABEL(SetPredictor):
         self.model.eval()
         self.device = model.device
         self.debug = debug
-
         if not isinstance(alpha, float):
             alpha = np.asarray(alpha)
         self.alpha = alpha
 
         self.t = None
 
-    def calibrate(self, cal_dataset: Subset):
+    def calibrate(self, cal_dataset: Union[Subset, Dict], weights=None, calibration_type="inductive"):
         """Calibrate the thresholds used to construct the prediction set.
 
         :param cal_dataset: Calibration set.
         :type cal_dataset: Subset
         """
-        cal_dataset = prepare_numpy_dataset(
-            self.model, cal_dataset, ["y_prob", "y_true"], debug=self.debug
-        )
+        assert (calibration_type == "mondrian" or calibration_type == "inductive", "Expection calibration type is inductive or mondrian. Got: {calibration_type}")
+        
+        if not isinstance(cal_dataset, dict):
+            cal_dataset = prepare_numpy_dataset(
+                self.model, cal_dataset, ["y_prob", "y_true"], debug=self.debug
+            )
+
         y_prob = cal_dataset["y_prob"]
         y_true = cal_dataset["y_true"]
 
+        if weights is None:
+            weights = [1.0 for i in range(len(y_prob))]
+            logging.warning("No weights specified for calibration. Calibrating with equal weights for all samples")
+        
+        assert (len(weights) == len(y_prob), f"Weights should be of same size as y_prob, Expected {len(y_prob)} weights has {len(weights)} values")
+
         N, K = cal_dataset["y_prob"].shape
-        if isinstance(self.alpha, float):
-            t = _query_quantile(y_prob[np.arange(N), y_true], self.alpha)
-        else:
-            t = [
-                _query_quantile(y_prob[y_true == k, k], self.alpha[k]) for k in range(K)
-            ]
+        if calibration_type == "inductive":
+            if isinstance(self.alpha, float):
+                t = _query_weighted_quantile(y_prob[np.arange(N), y_true], self.alpha, weights)
+            else:
+                t = [
+                    _query_weighted_quantile(y_prob[np.arange(N), y_true], self.alpha[k], weights) for k in range(K)
+                ]
+        
         self.t = torch.tensor(t, device=self.device)
+
 
     def forward(self, **kwargs) -> Dict[str, torch.Tensor]:
         """Forward propagation (just like the original model).
@@ -127,8 +150,26 @@ class LABEL(SetPredictor):
         :rtype: Dict[str, torch.Tensor]
         """
         pred = self.model(**kwargs)
-        pred["y_predset"] = pred["y_prob"] > self.t
+        pred["y_predset"] = pred["y_prob"].cpu() > self.t
         return pred
+
+
+class CoDrug(WeightedCP):
+    
+    def __init__(self, model: BaseModel, alpha: Union[float, np.ndarray], debug=False, **kwargs) -> None:
+        super().__init__(model, alpha, debug, **kwargs)
+
+    
+    def calibrate(self, cal_dataset: Subset, test_dataset: Subset, calibration_type="inductive"):
+        cal_preds = prepare_numpy_dataset(
+            self.model, cal_dataset, ["y_prob", "y_true"], debug=self.debug
+        )
+
+        test_preds = prepare_numpy_dataset(
+            self.model, test_dataset, ["y_prob", "y_true"], debug=self.debug
+        )
+        weights = compute_weights_for_codrug(cal_preds["y_prob"], test_preds["y_prob"])
+        return super().calibrate(cal_preds, weights, calibration_type)
 
 
 if __name__ == "__main__":
@@ -136,9 +177,10 @@ if __name__ == "__main__":
     from pyhealth.datasets import ISRUCDataset, split_by_patient, get_dataloader
     from pyhealth.models import SparcNet
     from pyhealth.tasks import sleep_staging_isruc_fn
-    from pyhealth.calib.predictionset import LABEL
+    from pyhealth.calib.predictionset.weighted_CP import WeightedCP
+    from pyhealth.calib.predictionset.weighted_CP import CoDrug
 
-    sleep_ds = ISRUCDataset("/srv/scratch/sl160/temp", dev=True, download=True).set_task(
+    sleep_ds = ISRUCDataset("/srv/scratch/sl160/temp", dev=True,download="True").set_task(
         sleep_staging_isruc_fn
     )
     train_data, val_data, test_data = split_by_patient(sleep_ds, [0.6, 0.2, 0.2])
@@ -147,12 +189,14 @@ if __name__ == "__main__":
     )
     # ... Train the model here ...
     # Calibrate the set classifier, with different class-specific mis-coverage rates
-    cal_model = LABEL(model, [0.15, 0.3, 0.15, 0.15, 0.15])
+    # cal_model = WeightedCP(model, [0.15, 0.3, 0.15, 0.15, 0.15])
+    cal_model = CoDrug(model, [0.15, 0.3, 0.15, 0.15, 0.15])
     # Note that I used the test set here because ISRUCDataset has relatively few
     # patients, and calibration set should be different from the validation set
     # if the latter is used to pick checkpoint. In general, the calibration set
     # should be something exchangeable with the test set. Please refer to the paper.
-    cal_model.calibrate(cal_dataset=test_data)
+    cal_model.calibrate(cal_dataset=val_data, test_dataset=test_data)
+
     # Evaluate
     from pyhealth.trainer import Trainer, get_metrics_fn
 
