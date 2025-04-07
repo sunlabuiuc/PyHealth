ecd218f253f36ab7cf0fa6e5adfade4b024e0fa5
zzachw <zw12@illinois.edu>
2025-04-07 00:40:32 -0500

    [6/N] Refactor model structure and introduce EmbeddingModel
    - Updated `BaseModel` to streamline initialization and remove deprecated parameters.
    - Introduced `EmbeddingModel` for handling embedding layers for various input types.
    - Refactored `RNN` class to utilize `EmbeddingModel` for embedding inputs, enhancing modularity.
    - Cleaned up unused code and improved type annotations for better clarity and maintainability.

--- pyhealth/models/__init__.py
+++ pyhealth/models/__init__.py
@@ -1,26 +1,28 @@
+# from .base_model import BaseModel
+# from .rnn import RNN, RNNLayer
+# from .transformer import Transformer, TransformerLayer
+# from .retain import RETAIN, RETAINLayer
+# from .cnn import CNN, CNNLayer
+# from .micron import MICRON, MICRONLayer
+# from .gamenet import GAMENet, GAMENetLayer
+# from .safedrug import SafeDrug, SafeDrugLayer
+# from .mlp import MLP
+# from .deepr import Deepr, DeeprLayer
+# from .contrawr import ResBlock2D, ContraWR
+# from .adacare import AdaCare, AdaCareLayer
+# from .concare import ConCare, ConCareLayer
+# from .agent import Agent, AgentLayer
+# from .grasp import GRASP, GRASPLayer
+# from .stagenet import StageNet, StageNetLayer
+# from .tcn import TCN, TCNLayer
+# from .sparcnet import SparcNet, DenseBlock, DenseLayer, TransitionLayer
+# from .molerec import MoleRec, MoleRecLayer
+# from .gnn import GCN, GAT
+# from .graph_torchvision_model import Graph_TorchvisionModel
+# from .medlink import MedLink
+# from .vae import VAE
+# from .gan import GAN
+
 from .base_model import BaseModel
+from .embedding import EmbeddingModel
 from .rnn import RNN, RNNLayer
-from .transformer import Transformer, TransformerLayer
-from .retain import RETAIN, RETAINLayer
-from .cnn import CNN, CNNLayer
-from .micron import MICRON, MICRONLayer
-from .gamenet import GAMENet, GAMENetLayer
-from .safedrug import SafeDrug, SafeDrugLayer
-from .mlp import MLP
-from .deepr import Deepr, DeeprLayer
-from .contrawr import ResBlock2D, ContraWR
-from .adacare import AdaCare, AdaCareLayer
-from .concare import ConCare, ConCareLayer
-from .agent import Agent, AgentLayer
-from .grasp import GRASP, GRASPLayer
-from .stagenet import StageNet, StageNetLayer
-from .tcn import TCN, TCNLayer
-from .sparcnet import SparcNet, DenseBlock, DenseLayer, TransitionLayer
-from .molerec import MoleRec, MoleRecLayer
-from .gnn import GCN, GAT
-from .graph_torchvision_model import Graph_TorchvisionModel
-from .medlink import MedLink
-from .vae import VAE
-from .gan import GAN
-from .torchvision_model import TorchvisionModel
-from .transformers_model import TransformersModel
