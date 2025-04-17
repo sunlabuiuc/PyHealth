bfd083abca2587711beb03dcdd73b1d9e25bdb87
Zhenbang Wu <32742524+zzachw@users.noreply.github.com>
2025-04-17 16:28:29 -0500

    Refactor COVID19CXRDataset (#337)
    * Refactor COVID19CXRDataset
    
    - Updated COVID19CXRDataset to the new API
    - Updated TorchvisionModel to the new API
    - Updated  ImageProcessor docstring
    - Added progress indication in SampleDataset during sample processing.
    
    * Add mode attribute to TorchvisionModel based on dataset output schema

--- pyhealth/datasets/covid19_cxr.py
+++ pyhealth/datasets/covid19_cxr.py
@@ -1,46 +1,53 @@
+import logging
 import os
-from collections import Counter
+from pathlib import Path
+from typing import Optional
 
 import pandas as pd
 
-from pyhealth.data import Patient
-from pyhealth.datasets import BaseDataset
-from pyhealth.tasks.covid19_cxr_classification import COVID19CXRClassification
+from ..tasks import COVID19CXRClassification
+from .base_dataset import BaseDataset
+
+logger = logging.getLogger(__name__)
 
 
 class COVID19CXRDataset(BaseDataset):
-    """Base image dataset for COVID-19 Radiography Database
+    """Base image dataset for COVID-19 Radiography Database.
 
-    Dataset is available at https://www.kaggle.com/datasets/tawsifurrahman/covid19-radiography-database
+    Dataset is available at:
+    https://www.kaggle.com/datasets/tawsifurrahman/covid19-radiography-database
 
-    **COVID-19 data:
-    -----------------------
-    COVID data are collected from different publicly accessible dataset, online sources and published papers.
-    -2473 CXR images are collected from padchest dataset[1].
-    -183 CXR images from a Germany medical school[2].
-    -559 CXR image from SIRM, Github, Kaggle & Tweeter[3,4,5,6]
-    -400 CXR images from another Github source[7].
+    Data Sources:
+    ------------
+    COVID-19 data:
+        - 2473 CXR images from padchest dataset[1]
+        - 183 CXR images from a Germany medical school[2]
+        - 559 CXR images from SIRM, Github, Kaggle & Tweeter[3,4,5,6]
+        - 400 CXR images from another Github source[7]
 
-    ***Normal images:
-    ----------------------------------------
-    10192 Normal data are collected from three different dataset.
-    -8851 RSNA [8]
-    -1341 Kaggle [9]
+    Normal images:
+        - 8851 from RSNA [8]
+        - 1341 from Kaggle [9]
 
-    ***Lung opacity images:
-    ----------------------------------------
-    6012 Lung opacity CXR images are collected from Radiological Society of North America (RSNA) CXR dataset  [8]
+    Lung opacity images:
+        - 6012 from Radiological Society of North America (RSNA) CXR dataset[8]
 
-    ***Viral Pneumonia images:
-    ----------------------------------------
-    1345 Viral Pneumonia data are collected from  the Chest X-Ray Images (pneumonia) database [9]
+    Viral Pneumonia images:
+        - 1345 from the Chest X-Ray Images (pneumonia) database[9]
 
-    Please cite the follwoing two articles if you are using this dataset:
-    -M.E.H. Chowdhury, T. Rahman, A. Khandakar, R. Mazhar, M.A. Kadir, Z.B. Mahbub, K.R. Islam, M.S. Khan, A. Iqbal, N. Al-Emadi, M.B.I. Reaz, M. T. Islam, “Can AI help in screening Viral and COVID-19 pneumonia?” IEEE Access, Vol. 8, 2020, pp. 132665 - 132676.
-    -Rahman, T., Khandakar, A., Qiblawey, Y., Tahir, A., Kiranyaz, S., Kashem, S.B.A., Islam, M.T., Maadeed, S.A., Zughaier, S.M., Khan, M.S. and Chowdhury, M.E., 2020. Exploring the Effect of Image Enhancement Techniques on COVID-19 Detection using Chest X-ray Images. arXiv preprint arXiv:2012.02238.
+    Citations:
+    ---------
+    If you use this dataset, please cite:
+    1. M.E.H. Chowdhury, T. Rahman, A. Khandakar, et al. "Can AI help in
+       screening Viral and COVID-19 pneumonia?" IEEE Access, Vol. 8, 2020,
+       pp. 132665-132676.
+    2. Rahman, T., Khandakar, A., Qiblawey, Y., et al. "Exploring the Effect
+       of Image Enhancement Techniques on COVID-19 Detection using Chest X-ray
+       Images." arXiv preprint arXiv:2012.02238.
 
-    **Reference:
-    [1] https://bimcv.cipf.es/bimcv-projects/bimcv-covid19/#1590858128006-9e640421-6711
+    References:
+    ----------
+    [1] https://bimcv.cipf.es/bimcv-projects/bimcv-covid19/
     [2] https://github.com/ml-workgroup/covid-19-image-repository/tree/master/png
     [3] https://sirm.org/category/senza-categoria/covid-19/
     [4] https://eurorad.org
@@ -51,61 +58,90 @@ class COVID19CXRDataset(BaseDataset):
     [9] https://www.kaggle.com/paultimothymooney/chest-xray-pneumonia
 
     Args:
-        dataset_name: name of the dataset.
-        root: root directory of the raw data. *You can choose to use the path to Cassette portion or the Telemetry portion.*
-        dev: whether to enable dev mode (only use a small subset of the data).
-            Default is False.
-        refresh_cache: whether to refresh the cache; if true, the dataset will
-            be processed from scratch and the cache will be updated. Default is False.
+        root: Root directory of the raw data containing the dataset files.
+        dataset_name: Optional name of the dataset. Defaults to "covid19_cxr".
+        config_path: Optional path to the configuration file. If not provided,
+            uses the default config in the configs directory.
 
     Attributes:
-        root: root directory of the raw data (should contain many csv files).
-        dataset_name: name of the dataset. Default is the name of the class.
-        dev: whether to enable dev mode (only use a small subset of the data).
-            Default is False.
-        refresh_cache: whether to refresh the cache; if true, the dataset will
-            be processed from scratch and the cache will be updated. Default is False.
+        root: Root directory of the raw data.
+        dataset_name: Name of the dataset.
+        config_path: Path to the configuration file.
 
     Examples:
+        >>> from pyhealth.datasets import COVID19CXRDataset
         >>> dataset = COVID19CXRDataset(
-                root="/srv/local/data/zw12/raw_data/covid19-radiography-database/COVID-19_Radiography_Dataset",
-            )
-        >>> print(dataset[0])
-        >>> dataset.stat()
-        >>> dataset.info()
+        ...     root="/path/to/covid19_cxr"
+        ... )
+        >>> dataset.stats()
+        >>> samples = dataset.set_task()
+        >>> print(samples[0])
     """
 
-    def process(self):
+    def __init__(
+        self,
+        root: str,
+        dataset_name: Optional[str] = None,
+        config_path: Optional[str] = None,
+    ) -> None:
+        if config_path is None:
+            logger.info("No config path provided, using default config")
+            config_path = (
+                Path(__file__).parent / "configs" / "covid19_cxr.yaml"
+            )
+        if not os.path.exists(os.path.join(root, "covid19_cxr-metadata-pyhealth.csv")):
+            self.prepare_metadata(root)
+        default_tables = ["covid19_cxr"]
+        super().__init__(
+            root=root,
+            tables=default_tables,
+            dataset_name=dataset_name or "covid19_cxr",
+            config_path=config_path,
+        )
+        return
+
+    def prepare_metadata(self, root: str) -> None:
+        """Prepare metadata for the COVID-19 CXR dataset.
+
+        Args:
+            root: Root directory containing the dataset files.
+
+        This method:
+        1. Reads metadata from Excel files for each class
+        2. Processes file paths and labels
+        3. Combines all data into a single DataFrame
+        4. Saves the processed metadata to a CSV file
+        """
         # process and merge raw xlsx files from the dataset
         covid = pd.DataFrame(
-            pd.read_excel(f"{self.root}/COVID.metadata.xlsx")
+            pd.read_excel(f"{root}/COVID.metadata.xlsx")
         )
         covid["FILE NAME"] = covid["FILE NAME"].apply(
-            lambda x: f"{self.root}/COVID/images/{x}.png"
+            lambda x: f"{root}/COVID/images/{x}.png"
         )
         covid["label"] = "COVID"
         lung_opacity = pd.DataFrame(
-            pd.read_excel(f"{self.root}/Lung_Opacity.metadata.xlsx")
+            pd.read_excel(f"{root}/Lung_Opacity.metadata.xlsx")
         )
         lung_opacity["FILE NAME"] = lung_opacity["FILE NAME"].apply(
-            lambda x: f"{self.root}/Lung_Opacity/images/{x}.png"
+            lambda x: f"{root}/Lung_Opacity/images/{x}.png"
         )
         lung_opacity["label"] = "Lung Opacity"
         normal = pd.DataFrame(
-            pd.read_excel(f"{self.root}/Normal.metadata.xlsx")
+            pd.read_excel(f"{root}/Normal.metadata.xlsx")
         )
         normal["FILE NAME"] = normal["FILE NAME"].apply(
             lambda x: x.capitalize()
         )
         normal["FILE NAME"] = normal["FILE NAME"].apply(
-            lambda x: f"{self.root}/Normal/images/{x}.png"
+            lambda x: f"{root}/Normal/images/{x}.png"
         )
         normal["label"] = "Normal"
         viral_pneumonia = pd.DataFrame(
-            pd.read_excel(f"{self.root}/Viral Pneumonia.metadata.xlsx")
+            pd.read_excel(f"{root}/Viral Pneumonia.metadata.xlsx")
         )
         viral_pneumonia["FILE NAME"] = viral_pneumonia["FILE NAME"].apply(
-            lambda x: f"{self.root}/Viral Pneumonia/images/{x}.png"
+            lambda x: f"{root}/Viral Pneumonia/images/{x}.png"
         )
         viral_pneumonia["label"] = "Viral Pneumonia"
         df = pd.concat(
@@ -116,34 +152,18 @@ class COVID19CXRDataset(BaseDataset):
         df = df.drop(columns=["FORMAT", "SIZE"])
         df.columns = ["path", "url", "label"]
         for path in df.path:
-            # assert os.path.isfile(os.path.join(self.root, path))
-            assert os.path.isfile(path)
-        # create patient dict
-        patients = {}
-        for index, row in df.iterrows():
-            patients[index] = Patient(
-                patient_id=str(index),
-                attr_dict=row.to_dict(),
-            )
-        return patients
-
-    def stat(self):
-        super().stat()
-        print(f"Number of samples: {len(self.patients)}")
-        count = Counter([v.attr_dict['label'] for v in self.patients.values()])
-        print(f"Number of classes: {len(count)}")
-        print(f"Class distribution: {count}")
+            assert os.path.isfile(path), f"File {path} does not exist"
+        df.to_csv(
+            os.path.join(root, "covid19_cxr-metadata-pyhealth.csv"),
+            index=False
+        )
+        return
 
     @property
-    def default_task(self):
+    def default_task(self) -> COVID19CXRClassification:
+        """Returns the default task for this dataset.
+
+        Returns:
+            COVID19CXRClassification: The default classification task.
+        """
         return COVID19CXRClassification()
-
-
-if __name__ == "__main__":
-    dataset = COVID19CXRDataset(
-        root="/home/zw12/data/raw_data/covid19-radiography-database/COVID-19_Radiography_Dataset",
-    )
-    print(list(dataset.patients.items())[0])
-    dataset.stat()
-    samples = dataset.set_task()
-    print(samples[0])
