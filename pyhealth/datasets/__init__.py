94820b764e63206b7999ba41099244ea06d46908
Ben Crumbacher <61884797+bcrumbacher@users.noreply.github.com>
2025-05-06 17:12:47 -0400

    Add dataset for DLH Class

--- pyhealth/datasets/__init__.py
+++ pyhealth/datasets/__init__.py
@@ -42,3 +42,4 @@ from .splitter import split_by_patient, split_by_sample, split_by_visit
 from .tuab import TUABDataset
 from .tuev import TUEVDataset
 from .utils import collate_fn_dict, collate_fn_dict_with_padding, get_dataloader
+from .covid19hospital import Covid19Hospital
