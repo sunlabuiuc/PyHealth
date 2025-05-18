e7c384e572d7b6e40484eaabe5608b7634c52eb6
EricSchrock <eric.schrock22@gmail.com>
2025-05-18 21:15:26 +0000

    Add logging handler

--- pyhealth/datasets/chestxray14.py
+++ pyhealth/datasets/chestxray14.py
@@ -305,6 +305,12 @@ class ChestXray14Dataset(BaseDataset):
 
 if __name__ == "__main__":
     logger.setLevel(logging.INFO)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str)
