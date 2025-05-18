5417f5b0bf331d7985375dcb620e19daac26c914
EricSchrock <eric.schrock22@gmail.com>
2025-05-18 23:48:23 +0000

    Fix multilabel test and remove debug print

--- pyhealth/unittests/test_datasets/test_chestxray14.py
+++ pyhealth/unittests/test_datasets/test_chestxray14.py
@@ -139,11 +139,10 @@ class TestChestXray14Dataset(unittest.TestCase):
         samples = self.dataset.set_task()
         self.assertEqual(len(samples), 10)
         for sample in samples:
-            print(sample)
             if '00000001_000.png' in sample['path']:
-                self.assertTrue(torch.equal(sample["labels"], torch.tensor([0.0, 0.0, 0.0, 0.0, 0.0])))
+                self.assertTrue(torch.equal(sample["labels"], torch.tensor([1.0, 0.0, 0.0, 0.0, 0.0])))
             elif '00000002_000.png' in sample['image']:
-                self.assertTrue(torch.equal(sample["path"], torch.tensor([1.0, 0.0, 0.0, 0.0, 0.0])))
+                self.assertTrue(torch.equal(sample["path"], torch.tensor([0.0, 0.0, 0.0, 0.0, 0.0])))
             elif '00000003_003.png' in sample['path']:
                 self.assertTrue(torch.equal(sample["labels"], torch.tensor([0.0, 0.0, 0.0, 1.0, 1.0])))
 
