6989978d1745ce768b5f8ab9c03513a3eec95e8d
John Wu <54558896+jhnwu3@users.noreply.github.com>
2025-09-09 08:25:22 -0700

    SDOH Coding Model (#538)
    * add sentence level sdoh multi-label classification model and test
    
    * revert test mask
    
    * sdoh: llama download HF api key; response parse test; fix inf test
    
    * doc
    
    * just added more details to the docs
    
    * readthedocs updates
    
    ---------
    
    Co-authored-by: Paul Landes <landes@mailc.net>
    Co-authored-by: John Wu <johnwu3@sunlab-serv-03.cs.illinois.edu>

---
+++ tests/core/test_sdoh.py
@@ -0,0 +1,36 @@
+from typing import Set
+from base import BaseTestCase
+from pyhealth.models.sdoh import SdohClassifier
+
+
+class TestSdoh(BaseTestCase):
+    def setUp(self):
+        self.set_random_seed()
+
+    def test_parse_reponse(self):
+        """Test the parsing of the SDOH output."""
+        import pandas as pd
+        df = pd.read_csv('test-resources/core/sdoh-test-cases.csv')
+        sdoh = SdohClassifier()
+        response: str
+        preds: str
+        for response, preds in df.itertuples(index=False, name=None):
+            parsed: str = ','.join(sorted(sdoh._parse_response(response)))
+            parsed = '-' if len(parsed) == 0 else parsed
+            self.assertEqual(parsed, preds)
+
+    def test_predict(self):
+        """Test SDOH prediction."""
+        # example sentence
+        sent = 'Pt is homeless and has no car and has no parents or support'
+        sdoh = SdohClassifier()
+        try:
+            preds: Set[str] = sdoh.predict(sent)
+            self.assertEqual({'housing', 'transportation'}, preds)
+        except OSError as e:
+            # but allow pass of the test if downloading the model on GitHub
+            # workflows fails to download the model
+            if str(e).find('You are trying to access a gated repo') > -1:
+                pass
+            else:
+                raise e
