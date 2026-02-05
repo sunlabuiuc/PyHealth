9cabac56e92ffa7fc87860e8040ed1055098b013
Yongda Fan <yongdaf2@illinois.edu>
2026-02-05 17:42:22 -0600

    define interface

--- pyhealth/models/base_model.py
+++ pyhealth/models/base_model.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import Callable, Any
+from typing import Callable, Any, Optional
 import inspect
 
 import torch
@@ -16,9 +16,20 @@ class BaseModel(ABC, nn.Module):
     Args:
         dataset (SampleDataset): The dataset to train the model. It is used to query certain
             information such as the set of all tokens.
+            
+    Interpretability
+    --------
+        To use a model with interpretability methods, the model must implement a method
+        `forward_from_embedding` that takes in embeddings as input instead of raw features;
+        for the models that already take in dense features as input, this method can simply
+        call the existing `forward` method. 
+        
+        For certain gradient-based interpretability methods (e.g., DeepLIFT), the model must also
+        ensure all non-linearity (e.g. ReLU, Sigmoid, Softmax) are using nn.Module versions instead of
+        functional versions (e.g., F.relu, F.sigmoid, F.softmax) so that hooks can be registered properly.
     """
 
-    def __init__(self, dataset: SampleDataset = None):
+    def __init__(self, dataset: Optional[SampleDataset] = None):
         """
         Initializes the BaseModel.
 
@@ -44,6 +55,49 @@ class BaseModel(ABC, nn.Module):
         self._dummy_param = nn.Parameter(torch.empty(0))
 
         self.mode = getattr(self, "mode", None)  # legacy API
+        
+    def forward(self, *kwargs: dict[str, torch.Tensor | str]) -> dict[str, torch.Tensor]:
+        """Forward pass of the model.
+        
+        Args:
+            *kwargs: A variable number of keyword arguments representing input features.
+                Each keyword argument is a tensor of shape (batch_size, ...).
+        
+        Returns:
+            A dictionary with the following keys:
+                distance: list of tensors of stage variation.
+                y_prob: a tensor of predicted probabilities.
+                loss [optional]: a scalar tensor representing the final loss, if self.label_keys in kwargs.
+                y_true [optional]: a tensor representing the true labels, if self.label_keys in kwargs.
+        """
+        raise NotImplementedError
+    
+    def forward_from_embedding(self, *kwargs: dict[str, torch.Tensor | str]) -> dict[str, torch.Tensor]:
+        """Forward pass of the model from embeddings.
+        
+        This method should be implemented for interpretability methods that require
+        access to the model's forward pass from embeddings.
+        
+        Args:
+            *kwargs: A variable number of keyword arguments representing input features
+                as embeddings. Each keyword argument is a tensor of shape (batch_size, ...).
+
+        Returns:
+            A dictionary with the following keys:
+                distance: list of tensors of stage variation.
+                y_prob: a tensor of predicted probabilities.
+                loss [optional]: a scalar tensor representing the final loss, if self.label_keys in kwargs.
+                y_true [optional]: a tensor representing the true labels, if self.label_keys in kwargs.
+        """
+        raise NotImplementedError
+
+    def get_embedding_model(self) -> nn.Module | None:
+        """Get the embedding model if applicable. This is used in pair with `forward_from_embedding`.
+
+        Returns:
+            nn.Module | None: The embedding model or None if not applicable.
+        """
+        raise NotImplementedError
 
     # ------------------------------------------------------------------
     # Internal helpers
@@ -94,6 +148,7 @@ class BaseModel(ABC, nn.Module):
         assert (
             len(self.label_keys) == 1
         ), "Only one label key is supported if get_output_size is called"
+        assert self.dataset is not None, "Dataset must be provided to get output size"
         output_size = self.dataset.output_processors[self.label_keys[0]].size()
         return output_size
 
@@ -113,6 +168,7 @@ class BaseModel(ABC, nn.Module):
         assert (
             len(self.label_keys) == 1
         ), "Only one label key is supported if get_loss_function is called"
+        assert self.dataset is not None, "Dataset must be provided to get loss function"
         label_key = self.label_keys[0]
         mode = self._resolve_mode(self.dataset.output_schema[label_key])
         if mode == "binary":
@@ -150,6 +206,7 @@ class BaseModel(ABC, nn.Module):
         assert (
             len(self.label_keys) == 1
         ), "Only one label key is supported if get_loss_function is called"
+        assert self.dataset is not None, "Dataset must be provided to prepare y_prob"
         label_key = self.label_keys[0]
         mode = self._resolve_mode(self.dataset.output_schema[label_key])
         if mode in ["binary"]:
