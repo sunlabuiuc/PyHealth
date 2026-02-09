e01a6598abc65ae4cd13d61cfe117a73d49715d1
Arjun Chatterjee <arj0jeechat@gmail.com>
2026-02-09 12:14:40 -0600

    [Conformal EEG] Neighborhood Conformal Prediction (#824)
    * NCP implementation
    
    * test script
    
    * All the init files
    
    * Check build
    
    * random unrealted fix to fix flaky test
    
    * Add docs
    
    * scripts for baseline testing
    
    * update data path
    
    * quick test script changes
    
    * fix nan error
    
    * Fixed ncp implementation
    
    * minor fixes
    
    * empty set logic
    
    * Math fix
    
    * Modify tests to reflect fix
    
    * Updated tests

--- pyhealth/calib/predictionset/__init__.py
+++ pyhealth/calib/predictionset/__init__.py
@@ -1,10 +1,18 @@
 """Prediction set construction methods"""
 
 from pyhealth.calib.predictionset.base_conformal import BaseConformal
-from pyhealth.calib.predictionset.cluster import ClusterLabel
+from pyhealth.calib.predictionset.cluster import ClusterLabel, NeighborhoodLabel
 from pyhealth.calib.predictionset.covariate import CovariateLabel
 from pyhealth.calib.predictionset.favmac import FavMac
 from pyhealth.calib.predictionset.label import LABEL
 from pyhealth.calib.predictionset.scrib import SCRIB
 
-__all__ = ["BaseConformal", "LABEL", "SCRIB", "FavMac", "CovariateLabel", "ClusterLabel"]
+__all__ = [
+    "BaseConformal",
+    "LABEL",
+    "SCRIB",
+    "FavMac",
+    "CovariateLabel",
+    "ClusterLabel",
+    "NeighborhoodLabel",
+]
