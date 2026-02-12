0e1df7711b9b240f1ae0673a112b39e6a440e564
William Pang <william.pang@aya.yale.edu>
2026-02-12 12:23:42 -0800

    Update multimodal_task.py

--- examples/foundation_ehr/multimodal_task.py
+++ examples/foundation_ehr/multimodal_task.py
@@ -11,7 +11,7 @@ from pyhealth.tasks.base_task import BaseTask
 # There's probably better ways dealing with this on the cluster, but working locally for now 
 # (see: https://github.com/sunlabuiuc/PyHealth/blob/master/examples/mortality_prediction/multimodal_mimic4_minimal.py)
 
-PYHEALTH_REPO_ROOT = #'/Users/wpang/Desktop/PyHealth'
+PYHEALTH_REPO_ROOT = '/Users/wpang/Desktop/PyHealth'
 
 EHR_ROOT = os.path.join(PYHEALTH_REPO_ROOT, "srv/local/data/physionet.org/files/mimiciv/2.2")
 NOTE_ROOT = os.path.join(PYHEALTH_REPO_ROOT, "srv/local/data/physionet.org/files/mimic-iv-note/2.2")
@@ -26,8 +26,8 @@ if __name__ == "__main__":
             ehr_tables=["diagnoses_icd", "procedures_icd", "prescriptions", "labevents"],
             note_tables=["discharge", "radiology"],
             cache_dir=CACHE_DIR,
-            num_workers=16,
-            dev=True
+            num_workers=8,
+            # dev=True
         )
     
     # Apply multimodal task
