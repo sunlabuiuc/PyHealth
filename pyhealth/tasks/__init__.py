90982927049b95c18ba51817698d9b4071001f72
diegofariasc <fariasco.diego@gmail.com>
2026-03-12 03:52:08 -0600

    feat: add SleepWakeClassification to init.py

--- pyhealth/tasks/__init__.py
+++ pyhealth/tasks/__init__.py
@@ -68,3 +68,5 @@ from .variant_classification import (
     VariantClassificationClinVar,
 )
 from .patient_linkage_mimic3 import PatientLinkageMIMIC3Task
+
+from .sleep_wake_classification import SleepWakeClassification
