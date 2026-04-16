20297dd740d270d055ab160c5fcb4b583140845a
Yiyun <yiyunw3@illinois.edu>
2026-04-15 22:04:20 -0700

    uncomment ecg task

--- pyhealth/tasks/__init__.py
+++ pyhealth/tasks/__init__.py
@@ -10,7 +10,7 @@ from .cardiology_detect import (
     cardiology_isWA_fn,
 )
 from .chestxray14_binary_classification import ChestXray14BinaryClassification
-# from .ecgqa_preprocess import ECGQA
+from .ecgqa_preprocess import ECGQA
 from .ptbxl_diagnostic_classification import PTBXLDiagnosticClassification
 from .chestxray14_multilabel_classification import ChestXray14MultilabelClassification
 from .covid19_cxr_classification import COVID19CXRClassification
