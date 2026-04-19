144c06a9d359df07cb0753ef0a1967cdb1432262
Kobe Guo <89816161+KobeGuo99@users.noreply.github.com>
2026-04-19 12:47:47 -0500

    dl4h final project kobeguo2 - CaliForest (#999)
    * dl4h final project kobeguo2 - CaliForest
    
    * Update CaliForest to require explicit fit before inference
    
    * Remove unused logit_scale from CaliForest

--- pyhealth/models/__init__.py
+++ pyhealth/models/__init__.py
@@ -45,3 +45,4 @@ from .text_embedding import TextEmbedding
 from .sdoh import SdohClassifier
 from .medlink import MedLink
 from .unified_embedding import UnifiedMultimodalEmbeddingModel, SinusoidalTimeEmbedding
+from .califorest import CaliForest
