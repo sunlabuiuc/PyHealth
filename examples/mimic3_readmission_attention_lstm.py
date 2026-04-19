470c2f22f65064a161245040e149a84012de3afc
Tianyu Zhou <tyzhou15@gmail.com>
2026-04-19 16:09:18 -0400

    Test Result

--- examples/mimic3_readmission_attention_lstm.py
+++ examples/mimic3_readmission_attention_lstm.py
@@ -19,13 +19,13 @@ from pyhealth.models import AttentionLSTM
 from pathlib import Path
 
 
-# run multiple seeds (run 100 times)
-SEEDS = list(range(1, 11))  
+# run multiple seeds (run 300 times)
+SEEDS = list(range(1, 301))  
 BATCH_SIZE = 32
-EPOCHS = 50
+EPOCHS = 5
 LR = 1e-3
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-OUTPUT_DIR = Path("results/mimic3_attention_10")
+OUTPUT_DIR = Path("results/mimic3_attention_300")
 TOPK = 10
 
 
